5b0434ed4963e5625c634a8e2c6d110167d53e98
黄标 <1475068541@qq.com>
2018-05-04 11:08:42 +0800

    commmit

--- ssmHello/src/main/java/com/roberthuang/ssm/controller/LoginController.java
+++ ssmHello/src/main/java/com/roberthuang/ssm/controller/LoginController.java
@@ -3,24 +3,29 @@ package com.roberthuang.ssm.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.roberthuang.ssm.bean.User;
 import com.roberthuang.ssm.service.ILoginservice;
 
-
 @Controller
-@RequestMapping(value = "/")  
+@RequestMapping(value = "/")
 public class LoginController {
 
-	 @Autowired  
-	  private ILoginservice loginService;  
-	      
-	 
-    @RequestMapping(value="/login",produces="text/html;charset=UTF-8" )   
-    @ResponseBody  
-    private String getOtherList(){  
-        User user =loginService.getUserById(6);  
-        return user==null?"用户不存在":user.toString();  
-    }  
+	@Autowired
+	private ILoginservice loginService;
+
+	@RequestMapping(value = "/login", produces = "text/html;charset=UTF-8")
+	@ResponseBody
+	private String getOtherList(@RequestParam int id) {
+		User user = loginService.getUserById(id);
+		return user == null ? "用户不存在" : user.toString();
+	}
+	
+	@RequestMapping(value = "/login.htm", produces = "text/html;charset=UTF-8")
+	private String getOtherList() {
+		
+		return "login";
+	}
 }
